b99d5c4a6412880a8eee0565955c30faf3d691a8
Ben Margolis <ben@sixpearls.com>
2014-12-21 20:32:45 -0800

    create migrations for django 1.7 compatibility

---
+++ blog/migrations/0001_initial.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import wagtail.wagtailcore.fields
+import datetime
+import modelcluster.tags
+import modelcluster.fields
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('wagtailimages', '0005_make_filter_spec_unique'),
+        ('taggit', '0001_initial'),
+        ('wagtailcore', '0010_change_page_owner_to_null_on_delete'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='BlogCategory',
+            fields=[
+                ('page_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='wagtailcore.Page')),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=('wagtailcore.page',),
+        ),
+        migrations.CreateModel(
+            name='BlogPost',
+            fields=[
+                ('page_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='wagtailcore.Page')),
+                ('content', wagtail.wagtailcore.fields.RichTextField(blank=True)),
+                ('date', models.DateField(default=datetime.datetime(2014, 12, 21, 20, 31, 45, 729828), help_text=b'The date used while organizing the posts')),
+                ('category', models.ForeignKey(related_name='blog_posts', blank=True, to='blog.BlogCategory', null=True)),
+                ('featured_image', models.ForeignKey(related_name='+', blank=True, to='wagtailimages.Image', null=True)),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=('wagtailcore.page',),
+        ),
+        migrations.CreateModel(
+            name='BlogPostTag',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('content_object', modelcluster.fields.ParentalKey(related_name='tagged_items', to='blog.BlogPost')),
+                ('tag', models.ForeignKey(related_name='blog_blogposttag_items', to='taggit.Tag')),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='BlogType',
+            fields=[
+                ('page_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='wagtailcore.Page')),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=('wagtailcore.page',),
+        ),
+        migrations.AddField(
+            model_name='blogpost',
+            name='tags',
+            field=modelcluster.tags.ClusterTaggableManager(to='taggit.Tag', through='blog.BlogPostTag', blank=True, help_text='A comma-separated list of tags.', verbose_name='Tags'),
+            preserve_default=True,
+        ),
+    ]
